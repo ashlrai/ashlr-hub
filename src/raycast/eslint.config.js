251f2f0f054899b8cf90e39ba35c4748ab4841ff
Mason Wyatt <masonwyatt23@users.noreply.github.com>
2026-08-10 16:35:45 -0400

    chore(raycast): bridge toolchain to TypeScript 6

--- src/raycast/eslint.config.js
+++ src/raycast/eslint.config.js
@@ -1,14 +1,8 @@
-const { FlatCompat } = require('@eslint/eslintrc');
-const js = require('@eslint/js');
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-});
+const raycast = require('@raycast/eslint-config');
 
 module.exports = [
   {
     ignores: ['dist/**', 'node_modules/**'],
   },
-  ...compat.extends('@raycast/eslint-config'),
+  ...raycast,
 ];
